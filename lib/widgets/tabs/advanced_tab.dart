c35e71f994a271cc95a3480a678e33d372e75a61
Prateek Rungta <prungta2@gmail.com>
2026-06-21 16:51:22 -0400

    fix: open config file on macOS

--- lib/widgets/tabs/advanced_tab.dart
+++ lib/widgets/tabs/advanced_tab.dart
@@ -201,12 +201,11 @@ class AdvancedTab extends ConsumerWidget {
                 final configPath = await configService.configPath;
                 final file = File(configPath);
 
-                if (await file.exists()) {
-                  Process.start('cmd.exe', ['/c', 'start', '', configPath]);
-                } else {
+                if (!await file.exists()) {
                   await configService.saveConfig(UserConfig());
-                  Process.start('cmd.exe', ['/c', 'start', '', configPath]);
                 }
+
+                await _openConfigFile(configPath);
               } catch (e) {
                 debugPrint('Error opening config file: $e');
               }
@@ -215,5 +214,17 @@ class AdvancedTab extends ConsumerWidget {
         ],
       ),
     );
+  }
+
+  Future<void> _openConfigFile(String configPath) {
+    if (Platform.isWindows) {
+      return Process.start('cmd.exe', ['/c', 'start', '', configPath]);
+    }
+
+    if (Platform.isMacOS) {
+      return Process.start('open', [configPath]);
+    }
+
+    return Process.start('xdg-open', [configPath]);
   }
 }
